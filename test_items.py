915ae75b8ce516d013f588ca3d929636572e6d77
Asja Makarowska <anutya@mail.ru>
2020-01-04 17:56:34 +0300

    modify conftest and test

--- test_items.py
+++ test_items.py
@@ -1,6 +1,10 @@
+import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_order_button_exstence(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+    # addbutton = browser.find_element_by_css_selector(".btn-add-to-basket")
+    # assert addbutton is True
+    # time.sleep(30)
