01af7e0083d085bd346f8bcf4fc61255547c8032
Asja Makarowska <anutya@mail.ru>
2020-01-06 14:47:51 +0300

    Change  test and conftest files, delete extra, alter readme

--- test_items.py
+++ test_items.py
@@ -1,10 +1,15 @@
 import time
+from selenium.webdriver.support import expected_conditions as EC
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_order_button_exstence(browser):
+def test_order_button_existence(browser):
     browser.get(link)
-    # addbutton = browser.find_element_by_css_selector(".btn-add-to-basket")
-    # assert addbutton is True
-    # time.sleep(30)
+    result = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert EC.visibility_of(result), "Button isn't visible or missing"
+    # change True to False to see TimeoutException
+    print('Result is ', result.text)
+    # Для проверки Ajouter au panier
+    time.sleep(30)
+    # assert result.text == 'Ajouter au panier', "Text is not the same"
